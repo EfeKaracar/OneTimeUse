90c4c76e19681624b78c93d97bfa653a69a7b624
EfeKaracar <efekaracar@gmail.com>
2021-07-12 12:32:49 -0700

    gitignore
    gitignore

---
+++ Assets/Scripts/itemBehaviours.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBehaviours : MonoBehaviour
+{
+    float cameraZdistance;
+    private void Start() {
+        cameraZdistance = Camera.main.WorldToScreenPoint(transform.position).z; // z axis of the game object
+    }
+    private void OnMouseDrag() {
+        Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZdistance);
+        Vector3 newWorldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        transform.position = newWorldPosition;
+    }
+}
---
+++ Assets/Scripts/spawnManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    public GameObject LeftSpawn;
+    public GameObject RightSpawn;
+
+    private void Start()
+    {
+        
+    }
+}
