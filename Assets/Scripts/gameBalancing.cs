a70dbbbaaed33b26af434eddbf0d822f98131906
EfeKaracar <efekaracar@gmail.com>
2021-07-12 13:44:54 -0700

    spawnManager
    spawnManager

---
+++ Assets/Scripts/gameBalancing.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class gameBalancing
+{
+    public static float itemMoveSpeed = 2f;
+}
--- Assets/Scripts/spawnManager.cs
+++ Assets/Scripts/spawnManager.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class spawnManager : MonoBehaviour
 {
+    public GameObject _prefabItem;
+    public List<GameObject> spawnedItems;
     public GameObject LeftSpawn;
     public GameObject RightSpawn;
+    
 
-    private void Start()
-    {
-        
+    private void Start() {
+        InvokeRepeating("spawnItem", 5, 2);
+    }
+
+    private void Update() {
+        if (spawnedItems.Count <= 0) return;
+        foreach(GameObject S in spawnedItems) {
+            S.transform.position = Vector3.MoveTowards(S.transform.position, RightSpawn.transform.position, Time.deltaTime * gameBalancing.itemMoveSpeed);
+        }
+    }
+
+    public void spawnItem() {
+        GameObject O = Instantiate(_prefabItem, LeftSpawn.transform.position, Quaternion.identity);
+        spawnedItems.Add(O);
     }
 }
